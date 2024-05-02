b5aac2812d36da4328a1a251ecb70cb58eeed07c
Eric Moore <ericmoore0709@gmail.com>
2024-05-01 17:43:20 -0700

    Created User model

---
+++ models.py
@@ -0,0 +1,25 @@
+"""Models for app."""
+
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'User[username={self.username}, email={self.email}]'
